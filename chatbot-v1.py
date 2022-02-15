0a3e8375eb02653ce95667aae94e68729335d3f3
kclisp <kenchen1368@gmail.com>
2022-02-15 00:20:35 -0500

    simplify code

--- chatbot-v1.py
+++ chatbot-v1.py
@@ -37,14 +37,13 @@ session_prompt = """誠は優しくて、協力的で、親切で、頭のいい
 誠: 一番楽しいことは、友達と一緒に過ごすことです。
 あなた: いい友達ですね
 誠: いい友達です。"""
-# \nあなた: 
+
 chat_log = session_prompt
 
-def chat(chat_input, chat_log=None):
-    prompt_text = f'{chat_log}{restart_sequence} {chat_input}{start_sequence}'
+def chat():
     response = openai.Completion.create(
       engine="text-curie-001",
-      prompt= prompt_text,
+      prompt= chat_log,
       temperature=0.7,
       max_tokens=64,
       top_p=1,
@@ -55,18 +54,13 @@ def chat(chat_input, chat_log=None):
     story = response['choices'][0]['text']
     return str(story)
 
-def append_interaction_to_chat_log(chat_input, bot_response, chat_log=None):
-    if chat_log == None:
-        chat_log = session_prompt
-    return f'{chat_log}{restart_sequence} {chat_input}{start_sequence}{bot_response}'
-
 print("""Welcome to the chatbot!
  NOTE: Please try not to use too many tokens for the chatbot (e.g. a whole paragraph or essay as input for each message) as that will start to reach my quota limit...
  But besides that, feel free to test out the bot! Press 'ctrl C' to stop the chat!
  Type a message to Makoto!""")
 while True:
     user_input = input()
-    chat_log = chat_log
-    bot_response = chat(user_input, chat_log)
-    chat_log = append_interaction_to_chat_log(user_input, bot_response, chat_log)
+    chat_log += restart_sequence + user_input + start_sequence
+    bot_response = chat()
+    chat_log += bot_response
     print(bot_response)
